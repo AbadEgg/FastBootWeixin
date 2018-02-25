485162465281657c67591697e35645b2cbce096a
amanda.shan <15824503913@163.com>
2018-02-25 10:44:55 +0800

    增加血压  血氧 心电分析数据的存储

---
+++ device_server/src/main/java/com/mlnx/device_server/service/SpoService.java
@@ -0,0 +1,48 @@
+package com.mlnx.device_server.service;
+
+import com.mlnx.local.data.store.spo.SpoStore;
+import com.mlnx.mp_session.domain.SpoInfo;
+import com.mlnx.mp_session.listenner.BroadCast;
+import com.mlnx.mp_session.listenner.spo.SpoListener;
+import com.mlnx.mptp.mptp.body.Topic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * Created by amanda.shan on 2018/2/24.
+ */
+@Service
+public class SpoService {
+
+    @Autowired
+    private SpoStore spoStore;
+
+    @PostConstruct
+    private void init() {
+        BroadCast.addSpoListener(spoListener);
+    }
+
+    private SpoListener spoListener = new SpoListener() {
+        @Override
+        public void reciveSpoInfo(List<Topic> topics, SpoInfo spoInfo) {
+            if (spoInfo.getResultSPO() != null && spoInfo.getResultSPO() != null && spoInfo.getPacketTime() != null) {
+                spoStore.saveBp(spoInfo.getResultSPO(), spoInfo.getResultHeart(), spoInfo.getPacketTime());
+            }
+        }
+
+        @Override
+        public void deviceOnline(Topic topic, String deviceId) {
+
+        }
+
+        @Override
+        public void deviceOfflien(Topic topic, String deviceId) {
+
+        }
+    };
+}
