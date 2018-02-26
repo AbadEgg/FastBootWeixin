81ed8edead03803a6dfa06269822d422342015b1
fzh <fzh000@163.com>
2018-02-26 13:55:54 +0800

    add by fzh

--- session_share_redis/src/test/java/com/mlnx/SessionShareRedisApplicationTests.java
+++ session_share_redis/src/test/java/com/mlnx/SessionShareRedisApplicationTests.java
@@ -1,7 +1,9 @@
 package com.mlnx;
 
+import com.mlnx.core.DeviceShare;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class SessionShareRedisApplicationTests {
 
+	@Autowired
+	private DeviceShare deviceShare;
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testSave(){
+		String deviceId = "test";
+		deviceShare.saveDevice(deviceId);
 	}
 
 }
