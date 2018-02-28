20d15ad4ecd62ec7379c4efd7d272e615b1c22a6
fzh <fzh000@163.com>
2018-02-28 17:21:12 +0800

    add by fzh

--- device_server/src/main/java/com/mlnx/device_server/DeviceServerApplication.java
+++ device_server/src/main/java/com/mlnx/device_server/DeviceServerApplication.java
@@ -4,8 +4,10 @@ import com.mlnx.analysis.EcgAnalysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.mlnx"})
 public class DeviceServerApplication {
 
 	public static void main(String[] args) {
